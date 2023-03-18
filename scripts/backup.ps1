4b1b310499d82121146d12f37d953edeb06c04f3
Wittano Bonarotti <radoslaw.ratyna@gmail.com>
2023-03-18 11:54:21 +0100

    feat(backup): check if new backup is duplicated

--- scripts/backup.ps1
+++ scripts/backup.ps1
@@ -1,28 +1,63 @@
 ﻿param(
     [string]$source,
+    [string]$filename,
     [string]$dest,
     [string]$alt
 )
 
-if([string]::IsNullOrEmpty($source) -or [string]::IsNullOrEmpty($dest)) {
+
+function Remove-Duplicate {
+    param (
+        [string]$filename,
+        [string]$dir
+    )
+
+    $first_saved_backup = Get-ChildItem -Path "$dir\$filename*" -File 
+    | Sort-Object LastWriteTime -Descending 
+    | Select-Object -First 1
+
+    $second_saved_backup = Get-ChildItem -Path "$dir\$filename*" -File 
+    | Sort-Object LastWriteTime -Descending 
+    | Select-Object -First 2
+    | Select-Object -Last 1
+
+    Write-Output "Calucating hashed for lastest backups"
+    $latest_update_hash = Get-FileHash $first_saved_backup
+    $saved_update_hash = Get-FileHash $second_saved_backup
+
+    if ($latest_update_hash -eq $saved_update_hash) {
+        Write-Output "Remove duplicate backups"
+        Remove-Item $first_saved_backup
+    }
+}
+
+if ([string]::IsNullOrEmpty($source) -or [string]::IsNullOrEmpty($dest)) {
     Write-Output "Missing required source or destination path"
     Exit
 }
 
-$data=(Get-Date -Format "dd-MM-yyyy")
-$basename = (Get-Item $source).BaseName
+$data = (Get-Date -Format "dd-MM-yyyy")
+
+if([string]::IsNullOrEmpty($filename)) {
+    $basename = (Get-Item $source).BaseName
+} else {
+    $basename = $filename
+}
+
 $destination = "$dest\$basename-backup-$data.zip"
 $alt_destination = "$alt\$basename-backup-$data.zip"
 
-Write-Output $destination
-
-If(-not (Test-Path $source)) {
+If (-not (Test-Path $source)) {
     Write-Output "Source directory '$source' not found"
     Exit
 }
 
-if(Test-Path $dest) {
+Write-Output "Create backup"
+if (Test-Path $dest) {
     Compress-Archive -LiteralPath $source -DestinationPath $destination
-} else {
+    Remove-Duplicate -dir $dest -filename $basename 
+}
+else {
     Compress-Archive -LiteralPath $source -DestinationPath $alt_destination
+    Remove-Duplicate -dir $alt -filename $basename 
 }
