1875883a4f4d6e45627c628cc31bd3ebc3d9ee66
Wittano <radoslaw.ratyna@gmail.com>
2023-11-13 20:10:04 +0100

    chore(osu): achived osu_backup.ps1

---
+++ scripts/osu_backup.ps1
@@ -0,0 +1,13 @@
+﻿$data=(Get-Date -Format "dd-MM-yyyy")
+$osu_directory = "C:\Users\radek\AppData\Local\osu!"
+$destination = "C:\Users\radek\Windows VM\osu!-" + $data + ".zip"
+
+If(-not (Test-Path $osu_directory)) {
+    Write-Output "osu! directory not found"
+    Exit
+}
+
+Add-Type -assembly "system.io.compression.filesystem" 
+[io.compression.zipfile]::CreateFromDirectory($osu_directory, $destination)
+
+Write-Output "done"
